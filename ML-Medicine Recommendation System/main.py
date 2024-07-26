751216b0acad7df81d97719c4e3f7db6e9f8938d
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-07-26 22:29:27 +0530

    Update main.py

--- ML-Medicine Recommendation System/main.py
+++ ML-Medicine Recommendation System/main.py
@@ -10,13 +10,12 @@ app = Flask(__name__)
 
 
 # load databasedataset===================================
-sym_des = pd.read_csv("symtoms_df.csv")
-precautions = pd.read_csv("precautions_df.csv")
-workout = pd.read_csv("workout_df.csv")
-description = pd.read_csv("description.csv")
-medications = pd.read_csv('medications.csv')
-diets = pd.read_csv("diets.csv")
-
+sym_des = pd.read_csv("datasets/symtoms_df.csv")
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv("datasets/medications.csv")
+diets = pd.read_csv("datasets/diets.csv")
 
 # load model===========================================
 svc = pickle.load(open('svc.pkl','rb'))

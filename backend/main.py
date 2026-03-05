1a52c6ff2e6c6c94e9879484790e03b5cd99b706
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2026-03-06 01:11:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -158,5 +158,9 @@ def get_symptoms():
 # RUN SERVER
 # ==============================
 
+# if __name__ == "__main__":
+
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)

ce29f8e1ff8a208899388b82bf246ac53b8698fe
Yash Bandal <147707204+Yash-Bandal@users.noreply.github.com>
2026-04-16 13:37:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -187,13 +187,13 @@ def get_symptoms():
 #     app.run(debug=True)
 
 #  - - Render
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
 
 
  # Railawy
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
